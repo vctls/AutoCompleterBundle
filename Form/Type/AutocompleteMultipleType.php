7f0315e9be43682c279faceacebe0e294d5febdc
toulouse <toulouse@insa-toulouse.fr>
2017-01-12 16:07:57 +0100

    testing collection transformer...

---
+++ Form/Type/AutocompleteMultipleType.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * User: toulouse
+ * Date: 2017-01-12
+ * Time: 15:26
+ */
+
+namespace PUGX\AutocompleterBundle\Form\Type;
+
+
+use PUGX\AutocompleterBundle\Form\Transformer\CollectionToIdsTransformer;
+
+class AutocompleteMultipleType extends AbstractType
+{
+    /**
+     * @var ManagerRegistry
+     */
+    private $registry;
+
+    /**
+     * @param ManagerRegistry $registry
+     */
+    public function __construct(ManagerRegistry $registry)
+    {
+        $this->registry = $registry;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $transformer = new CollectionToIdsTransformer($this->registry, $options['class']);
+        $builder->addModelTransformer($transformer);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'invalid_message' => 'The selected item does not exist',
+        ]);
+        $resolver->setRequired([
+            'class',
+        ]);
+        $resolver->setAllowedTypes('class', [
+            'string',
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return TextType::class;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'autocomplete';
+    }
+
+}
